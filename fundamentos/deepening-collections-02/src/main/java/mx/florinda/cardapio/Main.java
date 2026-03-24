9679a058ae744a9004299829cd4434ce9056f37e
Wagner O. Wutzke <wagner.wutzke@gmail.com>
2026-03-24 12:17:37 -0300

    Added project deepening-collections-02

--- fundamentos/deepening-collections-02/src/main/java/mx/florinda/cardapio/Main.java
+++ fundamentos/deepening-collections-02/src/main/java/mx/florinda/cardapio/Main.java
@@ -3,8 +3,7 @@ package mx.florinda.cardapio;
 import java.util.List;
 import java.util.Optional;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     static void main() {
         System.out.println("\n-------------------------");
